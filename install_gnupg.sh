a09f2d878f32a126625c1963afa608cbb363d6df
c0c0 <bnheruska@gmail.com>
2019-05-23 09:22:21 -0400

    added installation for .zshrc as well

--- install_gnupg.sh
+++ install_gnupg.sh
@@ -106,6 +106,16 @@ else
     cat .bash_gpgagent >> ~/.bashrc
 fi
 
+if [ -f ~/.zshrc]; then
+    cat ~/.zshrc | grep 'gpg-agent' 1>/dev/null 2>&1
+    if [ $? -eq 0 ]; then
+    echo "found 'gpg-agent' in .zshrc file already..."
+    else
+        echo "adding gpg-agent to .zshrc file"
+	cat .bash_gpgagent >> ~/.zshrc
+    fi
+fi
+
 echo "restarting agents"
 pkill ssh-agent
 pkill gpg-agent
